f5bb7a53955e457e1a81e30a95e888400459b149
Štefan Šimek <simek@triaxis.sk>
2020-09-22 09:47:56 +0200

    Unexpose specific ports

--- traefikswarm/commands/unexpose.py
+++ traefikswarm/commands/unexpose.py
@@ -2,12 +2,20 @@ from traefikswarm import Context
 
 def configure_argparser(parser):
     parser.add_argument('service', metavar='SERVICE', help='Service to un-expose')
+    parser.add_argument('port', metavar='PORT', help='Port to un-expose', nargs='?', type=int)
+    parser.add_argument('--router', help='Traefik router name override')
 
 def execute(ctx: Context):
-    name = ctx.args.service
+    args = ctx.args
+    name = args.service
     svc = ctx.get_service(name) if ctx.stackname else ctx.get_global_service(name)
 
     if not svc:
         ctx.abort(f'Service {name} not found')
 
-    svc.remove_labels('traefik')
+    if args.port or args.router:
+        router = args.router or f'{svc.name}-{args.port}'
+        for remove in [l for l in svc.labels if f'.{router}.' in l]:
+            svc.remove_label(remove)
+    else:
+        svc.remove_labels('traefik')
